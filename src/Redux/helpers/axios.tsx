4c5935afe932c63afccdd8790a9482107230e7a1
Jane49-cloud <janendirangu49@gmail.com>
2023-10-30 15:05:25 +0300

    debug

--- src/Redux/helpers/axios.tsx
+++ src/Redux/helpers/axios.tsx
@@ -1,24 +1,9 @@
 import axios from "axios";
 
 export const axiosService = axios.create({
-  baseURL: "http://13.245.28.232:8000",
+  baseURL: "http://13:8000",
 });
 
-// Add a response interceptor to handle errors
-// axiosService.interceptors.response.use(
-//   (response) => {
-//     return response;
-//   },
-//   (error) => {
-//     if (error.response && error.response.status === 400) {
-//       // Log the error response body
-//       console.error("Error Response Body:", error.response.data);
-//     }
-//     return Promise.reject(error);
-//   }
-// );
-
-// Add a request interceptor
 axiosService.interceptors.request.use(async (req) => {
   let token = localStorage.getItem("userToken");
   if (token) {
