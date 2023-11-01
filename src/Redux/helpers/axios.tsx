33c5e6100a6735d3dccf62d3ad2f427fada5c239
eleli <lesosioayub@gmail.com>
2023-11-01 17:48:47 +0300

    working live

--- src/Redux/helpers/axios.tsx
+++ src/Redux/helpers/axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosService = axios.create({
-  baseURL: "http://13.245.28.232:8000",
+  baseURL: "https://eleliafrika.com:8000",
 });
 
 axiosService.interceptors.request.use(async (req) => {
